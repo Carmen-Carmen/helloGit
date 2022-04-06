8f040aa06b1dfcc5cd01efac946d61f0dd1fa2de
xulingrui <lingrui_xu@icloud.com>
2022-04-06 16:09:11 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/lingrui/git/HelloGit.java
@@ -0,0 +1,13 @@
+package com.lingrui.git;
+
+/**
+ * @Description
+ * @Author Carmen
+ * @Date 2022-04-06 3:48 PM
+ * @Version 1.0
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello, Git!");
+    }
+}
